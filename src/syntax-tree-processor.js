b6bbd1770039418927ba3cbd6d642a551ffe3ad2
Matheus Torres <torresmiranda.rj@gmail.com>
2024-05-08 20:26:04 -0300

    feat: store & report current errors

--- src/syntax-tree-processor.js
+++ src/syntax-tree-processor.js
@@ -1,6 +1,9 @@
 export class SyntaxTreeProcessor {
   #filePath;
   #errors = new Map();
+  #messages = {
+    singleQuote: () => 'use single quotes instead of double quotes',
+  }
 
   static #validate(filePath) {
     if (!filePath || typeof filePath !== 'string') {
@@ -14,12 +17,22 @@ export class SyntaxTreeProcessor {
     this.#filePath = filePath;
   }
 
+  #storeError(message, { line, column }) {
+    const errorLocation = `${this.#filePath}:${line}:${column + 1}`;
+
+    this.#errors.set(errorLocation, { message, errorLocation });
+  }
+
   #handleLiteral(nodeDeclaration) {
     if (!(nodeDeclaration.raw && typeof nodeDeclaration.value === 'string')) return;
 
     if (!nodeDeclaration.raw.includes(`"`)) return;
 
-    nodeDeclaration.raw = nodeDeclaration.raw.replace(/"/g, "'")
+    nodeDeclaration.raw = nodeDeclaration.raw.replace(/"/g, "'");
+
+    const { line, column } = nodeDeclaration.loc.start;
+
+    this.#storeError(this.#messages.singleQuote(), { line, column });
   }
 
   #traverse(nodeDeclaration) {
@@ -31,7 +44,7 @@ export class SyntaxTreeProcessor {
 
     for (let key in nodeDeclaration) {
       if (typeof nodeDeclaration[key] !== 'object') continue;
-      
+
       this.#traverse(nodeDeclaration[key]);
     }
   }
