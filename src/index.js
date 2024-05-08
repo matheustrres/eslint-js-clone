d6d381bed24ee56cf061d03690f04a661ab69770
Matheus Torres <torresmiranda.rj@gmail.com>
2024-05-08 19:16:21 -0300

    feat: create getFilePathFromCLI function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('Hello World!');
+#!/usr/bin/env node
+
+import { readFileSync } from 'node:fs';
+import { parseArgs } from 'node:util';
+
+import chalk from 'chalk';
+import * as espree from 'espree';
+
+function getFilePathFromCLI() {
+  try {
+    const { values: { file } } = parseArgs({
+      options: {
+        file: {
+          type: 'string',
+          alias: 'f',
+        },
+      },
+    });
+
+    if (!file) {
+      throw new Error('Flag --file requires a file path to be specified.');
+    }
+
+    return file;
+  } catch (error) {
+    console.error(chalk.red('Error: Please provide a valid file path as an argument using -f or --file'));
+    process.exit(1);
+  }
+}
+
+const filePath = getFilePathFromCLI();
+const code = readFileSync(filePath, 'utf-8');
+
+const ast = espree.parse(code, {
+  ecmaVersion: 2020,
+  loc: true,
+  sourceType: 'module',
+});
+
+console.log(ast);
